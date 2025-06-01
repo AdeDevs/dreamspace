d9d8cce5f41bcb5946999d74c41c170324cb18ee
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-06-01 17:33:50 +0100

    Refactor styles: adjust padding and add main.css for new layout components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -72,6 +72,98 @@ export default function HomePage() {
                         <div className="create-img c-img3"></div>
                     </div>
                 </section>
+                <section className="unveiling">
+                    <h1>Unveiling The <br /> Extraordinary</h1>
+                    <div className="services">
+                        <div className="service-item s-item1">
+                            <h2>design expertise</h2>
+                            <p>With a diverse portfolio of successful projects, <br /> we possess the expertise to handle a wide <br /> range of design challenges</p>
+                        </div>
+                        <div className="service-item s-item2">
+                            <h2>client-centric approach</h2>
+                            <p>Your satisfaction is our priotity. We collaborate <br /> closely with you, ensuring your preferences <br /> and needs are integrated seamlessly</p>
+                        </div>
+                        <div className="service-item s-item3"></div>
+                        <div className="service-item s-item4">
+                            <h2>innovative solutions</h2>
+                            <p>We're committed to staying ahead of design <br /> trends and technology. Our designs combine <br /> timeless elegance.</p>
+                        </div>
+                        <div className="service-item s-item5">
+                            <h2>tailored budgeting</h2>
+                            <p>We understand that every project comes with <br /> a unique budget. Our transparent pricing <br /> ensures that you receive the best</p>
+                        </div>
+                    </div>
+                </section>
+                <section className="portfolio">
+                    <div className="p-head">
+                        <h1>where dreams <br /> take shape</h1>
+                        <p>
+                            Explore our portfolio to witness the magic we've created for our clients. From minimalistic chics to opulent luxury, we've turned dreams into stunning reality.
+                        </p>
+                    </div>
+                    <div className="projects">
+                        <div className="project">
+                            <div className="p-img p-img1"></div>
+                            <h2>contemporary chic apartment</h2>
+                            <p>
+                                In this project, we were entrusted with the task of turning a mundane urban apartment into a contemporary oasis. We embraced clean lines, subtle colors palettes, and strategic...
+                            </p>
+                            <button><ion-icon name="arrow-forward-outline"></ion-icon></button>
+                        </div>
+                        <div className="project">
+                            <div className="p-img p-img2"></div>
+                            <h2>Rustic retreat in the suburbs</h2>
+                            <p>
+                                Nestled in the heart of nature, this project allowed us to embrace the rustic charm of the countryside while infusing it with a touch of modern comfort. By using natural materials...
+                            </p>
+                            <button><ion-icon name="arrow-forward-outline"></ion-icon></button>
+                        </div>
+                        <div className="project">
+                            <div className="p-img p-img3"></div>
+                            <h2>minimalist serenity spa</h2>
+                            <p>
+                                Our expertise isn't limited to residential spaces; we also excel in commercial design. The Minimalist Serenity Spa project exemplifies our ability to create a serene ambiance that fosters relaxation...
+                            </p>
+                            <button><ion-icon name="arrow-forward-outline"></ion-icon></button>
+                        </div>
+                        <div className="project">
+                            <div className="p-img p-img4"></div>
+                            <h2>timeless elegance in a classic home</h2>
+                            <p>
+                                Preserving the classic charm of a heritage home while infusing it with modern functionalites is an art form we excel in. The Timeless Elegance project was a journey through history and time...
+                            </p>
+                            <button><ion-icon name="arrow-forward-outline"></ion-icon></button>
+                        </div>
+                    </div>
+                    <button className="work-btn">view all works <span><ion-icon name="arrow-forward-outline"></ion-icon></span></button>
+                </section>
+                {/* <section className="reviews">
+                    <h1>Don't just take our <br /> word for it</h1>
+                    <div className="review-img">
+                        <div className="r-avt"></div>
+                        <div className="r-avt"></div>
+                        <div className="r-avt"></div>
+                    </div>
+                    <div className="testimonials">
+                        <h2></h2>
+                        <p></p>
+                        <ul className="stars">
+                            <li><ion-icon name="star"></ion-icon></li>
+                            <li><ion-icon name="star"></ion-icon></li>
+                            <li><ion-icon name="star"></ion-icon></li>
+                            <li><ion-icon name="star"></ion-icon></li>
+                            <li><ion-icon name="star"></ion-icon></li>
+                        </ul>
+                        <p></p>
+                        <ul className="controller">
+                            <li className="nav-btn"><ion-icon name="arrow-back"></ion-icon></li>
+                            <li className="nav-selector nav-special"></li>
+                            <li className="nav-selector"></li>
+                            <li className="nav-selector"></li>
+                            <li className="nav-btn"><ion-icon name="arrow-forward"></ion-icon></li>
+                        </ul>
+                    </div>
+                </section> */}
             </main>
         </div>
     )
