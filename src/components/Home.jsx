4d89b11f019c6920c6b829f4ac6af5abc21751ec
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-06-01 14:54:03 +0100

    updt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -41,6 +41,38 @@ export default function HomePage() {
                 </section>
                 <section className="hero-item h-four"></section>
             </header>
+            <main className="home-main">
+                <section className="creating">
+                    <h1>Creating Environments <br /> Tailored to You</h1>
+                    <div className="create-item c-item1">
+                        <div className="create-txt">
+                            <h2>Commercial Design</h2>
+                            <p>
+                                Impress clients and boost productivity with our <br /> innovative commercial design solutions.
+                            </p>
+                        </div>
+                        <div className="create-img c-img1"></div>
+                    </div>
+                    <div className="create-item c-item2">
+                        <div className="create-txt">
+                            <h2>Residential Design</h2>
+                            <p>
+                                From living rooms to bedrooms, kitchens to bathrooms, <br /> we'll curate designs that resonate with your style.
+                            </p>
+                        </div>
+                        <div className="create-img c-img2"></div>
+                    </div>
+                    <div className="create-item c-item3">
+                        <div className="create-txt">
+                            <h2>Interior Styling</h2>
+                            <p>
+                                Our team will breathe life into your existing decor, <br /> making subtle yet impactful changes.
+                            </p>
+                        </div>
+                        <div className="create-img c-img3"></div>
+                    </div>
+                </section>
+            </main>
         </div>
     )
 }
